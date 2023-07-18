5b25f7387b1e96678e6837d11b1dbd47e8f82fde
Varvara Bondarenko <varvara.varejka2014@gmail.com>
2023-07-17 19:51:00 -0500

    new

--- routes/admin.js
+++ routes/admin.js
@@ -2,28 +2,17 @@ const path = require("path");
 
 const express = require("express");
 
-const rootDir = require("../util/path");
+const adminController = require("../controllers/admin");
 
 const router = express.Router();
 
-const products = [];
-
 // /admin/add-product => GET
-router.get("/add-product", (req, res, next) => {
-  res.render("add-product", {
-    pageTitle: "Add Product",
-    path: "/admin/add-product",
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true,
-  });
-});
+router.get("/add-product", adminController.getAddProduct);
 
-// /admin/add-poduct => POST
-router.post("/add-product", (req, res, next) => {
-  products.push({ title: req.body.title });
-  res.redirect("/");
-});
+// /admin/products => GET
+router.get("/products", adminController.getProducts);
 
-exports.routes = router;
-exports.products = products;
+// /admin/add-product => POST
+router.post("/add-product", adminController.postAddProduct);
+
+module.exports = router;
